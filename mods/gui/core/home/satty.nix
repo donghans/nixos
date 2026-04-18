443277a11af77aadcaa9e9696aed1130f59387e0
신동한 <78710114+donghans@users.noreply.github.com>
2026-04-18 12:43:13 +0900

    server 환경에서 Screenshots 폴더가 생성되지 않게 처리

--- mods/gui/core/home/satty.nix
+++ mods/gui/core/home/satty.nix
@@ -1,4 +1,9 @@
-_: {
+{
+  config,
+  lib,
+  ...
+}:
+lib.mkIf config.mods.gui.enable {
   xdg.configFile."satty/config.toml".text = ''
     [general]
     output-filename = "~/Pictures/Screenshots/satty-%Y%m%d_%H%M%S.png"
