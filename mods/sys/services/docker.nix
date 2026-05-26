73fe799835761a0213f202f3236c1e35adbcb22e
신동한 <78710114+donghans@users.noreply.github.com>
2026-05-26 15:57:04 +0900

    Update docker.nix

--- mods/sys/services/docker.nix
+++ mods/sys/services/docker.nix
@@ -21,6 +21,7 @@ mkMod __curPos "Docker Daemon and tools" ({cfg, ...}: {
       virtualisation.docker.autoPrune.enable = lib.mkIf (!cfg.rootless) true;
       virtualisation.docker.rootless.enable = lib.mkIf cfg.rootless true;
       virtualisation.docker.rootless.setSocketVariable = lib.mkIf cfg.rootless true;
+      virtualisation.docker.rootless.daemon.settings = lib.mkIf cfg.rootless {dns = ["8.8.8.8" "8.8.4.4"];};
       users.users.${config.workspace.username}.extraGroups = lib.mkIf (!cfg.rootless) ["docker"];
       boot.kernel.sysctl."net.ipv4.ip_unprivileged_port_start" = lib.mkIf cfg.rootless 80;
     }
@@ -28,6 +29,7 @@ mkMod __curPos "Docker Daemon and tools" ({cfg, ...}: {
     # 그리고 veth/브리지 인터페이스를 systemd-networkd가 가로채지 않도록 20번으로 명시 제외
     (lib.mkIf (!cfg.rootless) {
       networking.nftables.enable = true;
+      virtualisation.docker.daemon.settings.dns = ["8.8.8.8" "8.8.4.4"];
       systemd.network.networks."20-docker-veth" = {
         matchConfig.Name = "veth* br-* docker*";
         linkConfig.Unmanaged = true;
